2d882efd6bb8e2725f1c97e53519ba6b366810aa
Rado Jedro <radoslaw.jedro@gmail.com>
2019-10-26 17:48:16 +0100

    Add first props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from './List.scss'
 import Hero from '../Hero/Hero.js'
+import PropTypes from 'prop-types';
+
 
 class List extends React.Component {
-  render() {
+    
+    static propTypes = {
+      title: PropTypes.node.isRequired,
+      children: PropTypes.node.isRequired,
+    }
+    /*default props */
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+      }
+
+    render() {
     return (
       <section className={styles.component}>
         <h2 className={styles.component}>
-            <Hero />
+            <Hero titleText={this.props.title} />
+            <div className={styles.description}>
+                {this.props.children}
+            </div>
         </h2>
       </section>
     )
