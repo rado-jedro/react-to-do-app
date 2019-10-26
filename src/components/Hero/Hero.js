11492508c955dab992faa5389cfe478b99dc1964
Rado Jedro <radoslaw.jedro@gmail.com>
2019-10-26 19:27:32 +0100

    Add image props

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,12 +6,13 @@ import PropTypes from 'prop-types';
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{props.titleText}</h2>
-    <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
+    <img className={styles.image} src={props.heroImage} />
   </header>
 );
 
 Hero.propTypes = {
     titleText: PropTypes.node.isRequired,
+    heroImage: PropTypes.text
   };
 
 export default Hero;
