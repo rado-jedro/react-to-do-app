12bffdc14b00c225258e47b2005d1ae1d9dacd39
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-09 15:48:14 +0000

    Remove key:icon and cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -32,8 +32,6 @@ class Column extends React.Component {
           {
             key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
             title,
-            //icon: 'list-alt',
-            //cards: []
           }
         ]
       }
