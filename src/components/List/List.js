669b181aeb98b53eaa11d9630b9a72cc7e3e8b5f
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-10 22:32:42 +0000

    Add columns redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-//import Column from '../Column/Column.js';
+import Column from '../Column/ColumnContainer.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 //import Creator from '../Creator/Creator.js';
@@ -45,7 +45,7 @@ class List extends React.Component {
     }
 */
     render() {
-      const {title, image, description} = this.props;
+      const {title, image, description,columns} = this.props;
 
       return (
         <section className={styles.component}>
@@ -55,13 +55,13 @@ class List extends React.Component {
               {ReactHtmlParser(description)}
             </div>
           </h2>
-          {/*
+          
           <div className={styles.columns} >
-            {this.state.columns.map(({key, ...columnProps}) => (
-              <Column key={key} {...columnProps} />
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
             ))}
           </div>
-
+          {/*
           <div className={styles.creator}>
             <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>
